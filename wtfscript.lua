18f5173c7a086366224eff29c0ba31170ed0020b
Bail-03r <63909387+Bail-03r@users.noreply.github.com>
2021-12-13 21:14:36 +0300

    Update wtfscript.lua

--- wtfscript.lua
+++ wtfscript.lua
@@ -4,6 +4,6 @@ end
 if game.PlaceId == 301549746 then
     loadstring(game:HttpGet("https://pastebin.com/raw/qDrQNMSh", true))()
 end
-if game.PlaceId == id then
+if game.PlaceId == 5602055394 then
    loadstring(game:HttpGet('https://raw.githubusercontent.com/Bail-03r/bqhaxSense/main/daboobs.lua'))()
 end
